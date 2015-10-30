0e75f17f7ed93191f4739a42eec1f75bbc8336d4
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-30 13:55:51 +0530

    Use isomorphic fetch

--- src/__tests__/courier.js
+++ src/__tests__/courier.js
@@ -1,37 +1,77 @@
-import {describe, it, before} from 'mocha';
-import {Server} from 'hapi';
-import {Courier} from '../courier.js';
+import assert from 'assert';
+import {describe, it, beforeEach, afterEach} from 'mocha';
+import {server, counter} from './test_server.js';
+import Courier from '../courier.js';
 
 describe('Courier', () => {
-  let server;
-  const headers = {};
   const endpoint = 'http://localhost:8000';
-  const courier = new Courier({endpoint, headers});
+  const courier = new Courier(endpoint);
 
-  // TODO move the server to another test module
-  // and add methods to make it return errors
+  beforeEach(() => {
+    counter.reset();
+    return server.startAsync();
+  });
 
-  before(done => {
-    server = new Server();
-    server.connection({host: 'localhost', port: 8000});
-    server.start(done);
-    server.route({
-      method: 'POST',
-      path: '/',
-      handler: (request, reply) => {
-        reply({foo: 'bar'});
-      },
-    });
+  afterEach(() => {
+    return server.stopAsync();
   });
 
   describe('post', () => {
-    it('should post given data', async () => {
-      const [ res, body ] = await courier.post('test');
-      console.log('res, body', res, body);
+    it('should post given data', async function() {
+      const req = {type: 'echo'};
+      const res = await courier.send(req);
+      assert.equal(counter.value(), 1);
+      assert.deepEqual(res, req);
     });
 
-    it('should retry if unreachable');
-    it('should retry if 5xx error');
-    it('should not retry if 4xx error');
+    it('should retry if unreachable', async function() {
+      let erred = false;
+      this.timeout(6e4);
+
+      // stop the http server
+      await server.stopAsync();
+
+      try {
+        const req = {foo: 'bar'};
+        await courier.send(req);
+      } catch (e) {
+        assert.equal(e.message, 'reached maximum retry limit');
+        erred = true;
+      }
+
+      assert(erred);
+      assert.equal(counter.value(), 0);
+    });
+
+    it('should retry if 5xx error', async function() {
+      let erred = false;
+      this.timeout(6e4);
+
+      try {
+        const req = {type: 'e500'};
+        await courier.send(req);
+      } catch (e) {
+        assert.equal(e.message, 'reached maximum retry limit');
+        erred = true;
+      }
+
+      assert(erred);
+      assert.equal(counter.value(), 5);
+    });
+
+    it('should not retry if 4xx error', async function() {
+      let erred = false;
+
+      try {
+        const req = {type: 'e400'};
+        await courier.send(req);
+      } catch (e) {
+        assert.equal(e.message, 'failed by client error: 400');
+        erred = true;
+      }
+
+      assert(erred);
+      assert.equal(counter.value(), 1);
+    });
   });
 });
--- src/courier.js
+++ src/courier.js
@@ -1,23 +1,55 @@
-import Promise from 'bluebird';
+import fetch from 'isomorphic-fetch';
 
-import request from 'request';
-const post = Promise.promisify(request.post, {multiArgs: true});
+// return error after retries
+export const MAX_RETRIES = 5;
 
-class Courier {
-  constructor(options) {
-    this._options = {
-      json: true,
-      uri: options.endpoint,
-      headers: options.headers,
-    };
+export default class Courier {
+  constructor(uri) {
+    this._uri = uri;
   }
 
-  post(uri, body) {
-    const options = Object.assign({}, this._options, {body});
-    return post(options);
+  send(data) {
+    return new Promise((resolve, reject) => {
+      let retries = 0;
+      let body;
+
+      try {
+        body = JSON.stringify(data);
+      } catch (e) {
+        reject(e);
+      }
+
+      // try to send data to the server
+      // check retry limit before trying
+      const attempt = () => {
+        if (retries++ >= MAX_RETRIES) {
+          reject(new Error('reached maximum retry limit'));
+          return;
+        }
+
+        // backoff 0, 500, 2000, 4500, 8000, ...
+        const millis = 500 * Math.pow(retries, 2);
+
+        setTimeout(() => {
+          fetch(this._uri, {method: 'POST', body})
+            .then(validate)
+            .catch(attempt);
+        }, millis);
+      };
+
+      // validate response from the server
+      // if necessary, try sending again
+      const validate = res => {
+        if (res.status === 200) {
+          res.json().then(resolve);
+        } else if (res.status >= 400 && res.status < 500) {
+          reject(new Error('failed by client error: ' + res.status));
+        } else {
+          attempt();
+        }
+      };
+
+      attempt();
+    });
   }
 }
-
-export default {
-  Courier,
-};
